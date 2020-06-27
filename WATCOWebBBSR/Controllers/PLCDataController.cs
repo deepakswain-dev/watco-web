c415bf53b0c6934582254125a1475cf3f5855501
deepak <deepak.swain.ext@siemens-healthineers.com>
2020-06-28 01:23:02 +0530

    Controller added

---
+++ WATCOWebBBSR/Controllers/PLCDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WATCOWebBBSR.Controllers
+{
+    public class PLCDataController : Controller
+    {
+        // GET: PLCData
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
